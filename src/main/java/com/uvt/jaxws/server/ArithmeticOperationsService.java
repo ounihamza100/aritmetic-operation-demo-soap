52fe8f3b7ee42248aaf4b2c17f375dc89c20425d
ounihamza100 <hamza.ellouni@gmail.com>
2020-01-07 17:42:18 +0100

    polish

--- src/main/java/com/uvt/jaxws/server/ArithmeticOperationsService.java
+++ src/main/java/com/uvt/jaxws/server/ArithmeticOperationsService.java
@@ -1,7 +1,18 @@
 package com.uvt.jaxws.server;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
 /**
  * @author Hamza Ouni
  */
-public class ArithmeticOperationsService {
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface ArithmeticOperationsService {
+
+    @WebMethod(operationName = "add")
+    public Long add(String p, String p2);
+
+
 }
--- src/main/java/com/uvt/jaxws/server/ArithmeticOperationsServiceImpl.java
+++ src/main/java/com/uvt/jaxws/server/ArithmeticOperationsServiceImpl.java
@@ -1,7 +1,17 @@
 package com.uvt.jaxws.server;
 
+import javax.jws.WebService;
+
 /**
  * @author Hamza Ouni
  */
-public class ArithmeticOperationsServiceImpl {
+@WebService(endpointInterface = " com.uvt.jaxws.server.ArithmeticOperationsService", portName = "ArithmeticOperationsPort")
+public class ArithmeticOperationsServiceImpl implements ArithmeticOperationsService {
+
+    @Override
+    public Long add(String p1, String p2) {
+        Long r1 = Long.valueOf(p1);
+        Long r2 = Long.valueOf(p2);
+        return r1 + r2;
+    }
 }
